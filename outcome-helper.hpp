a98c291c4817983bee5eaa0a7ebb2bbd95f42966
Bogdan Vaneev <warchantua@gmail.com>
2019-03-20 21:41:57 +0200

    Add example
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

---
+++ outcome-helper.hpp
@@ -0,0 +1,9 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef EXAMPLE_OUTCOME_HELPER_HPP
+#define EXAMPLE_OUTCOME_HELPER_HPP
+
+#endif //EXAMPLE_OUTCOME_HELPER_HPP
